b96e1f6bf5033eab912f0b8743925973195ad1b0
katelynpeterson <33292506+katelynpeterson@users.noreply.github.com>
2019-04-15 15:41:54 -0600

    Added validation statements to Name class. Added unit test project. Removed unneeded project

---
+++ UI-Checkout/Name.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Name
+{ 
+	public Name(string name)
+	{
+        //Validation
+        //Origin???
+
+        //Length
+        if (name != null && name.Length() <= 20)
+        {
+            //Lexical
+            Regex lexicalRegex = new Regex(@"[A-Za-z]\s+*", RegexOptions.Compiled|RegexOptions.IgnoreCase);
+            MatchCollection match = lexicalRegex.Matches(name);
+            if(match.Count != 0)
+            {
+                //Syntax
+                Regex syntaxRegex = new Regex(@"[A-Z][a-z]\s+*", RegexOptions.Compiled);
+                match = syntaxRegex.Matches(name);
+                if (match.Count != 0)
+                {
+                    //Semantics???
+
+                    name = Name;
+                }
+            }
+        }
+	}
+
+    private Name name;
+    public Name Name
+    {
+        get { return name; }
+        set { SetField(ref name, value); }
+    }
+
+}
---
+++ UI-Checkout/UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void validateName()
+        {
+            string customerFirstname = "John";
+            Name firstname = new Name(customerFirstname);
+
+            
+        }
+    }
+}
