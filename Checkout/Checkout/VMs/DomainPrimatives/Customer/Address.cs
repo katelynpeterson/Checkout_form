aca07692dfe4f4b93d0454e9901b42a9f942853c
Michael Lewellen <michael.lewellen@snow.edu>
2019-04-16 22:26:55 -0600

    Defined many classes, validated many strings

---
+++ Checkout/Checkout/VMs/DomainPrimatives/Customer/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.VMs.DomainPrimatives.Customer
+{
+    public class Address 
+    {
+        public Address (string str, string c, string st, string z)
+        {
+            Street street = new Street(str);
+            City city = new City(c);
+            State state = new State(st);
+            Zip zip = new Zip(z);
+        }
+        
+    }
+}
---
+++ Checkout/Checkout/VMs/DomainPrimatives/Customer/EmailAddress.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Checkout.VMs.DomainPrimatives.Customer
+{
+    public class EmailAddress
+    {
+        public EmailAddress (String s)
+        {
+            if (validateEmail(s))
+            {
+                // Make all the characters lower case
+                s = s.ToLower();
+                Email = s;
+            }
+                
+            else
+                failGracefully();
+        }
+
+        private string email;
+        public string Email
+        {
+            get; private set;
+        }
+
+        public Boolean validateEmail(String s)
+        {
+            // Check length
+            if (s.Length > 30)
+            {
+                //Display string to long message
+                return false;
+            }
+            // Check characters
+            if (Regex.IsMatch(s, @"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b"))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void failGracefully()
+        {
+            return;
+        }
+    }
+}
+}
