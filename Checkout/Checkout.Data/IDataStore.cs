6a903b3004676ef69d97c9155f18b604e9631088
katelynpeterson <33292506+katelynpeterson@users.noreply.github.com>
2019-04-17 22:35:13 -0600

    Add classes and projects for database

---
+++ Checkout/Checkout.Data/IDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Checkout.VMs.Entity;
+
+namespace Checkout.Data
+{
+    public interface IDataStore
+    {
+        void AddCustomer(Customer c);
+        IEnumerable<Customer> GetAllCustomers();
+    }
+}
--- Checkout/Checkout/VMs/CheckoutVM.cs
+++ Checkout/Checkout/VMs/CheckoutVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Checkout.VMs
 {
-    class CheckoutVM
+    public class CheckoutVM
     {
     }
 }
